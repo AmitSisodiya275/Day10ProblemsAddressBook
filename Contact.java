9cc5615406037f0be3ca16434b6b8ece34ce1591
Amit Sisodiya <amitsisodiya275@gmail.com>
2021-10-03 12:22:00 +0530

    Added: a new file for UC3 in which lots of new features like you can now edit the existing contact details, search for a specific person details and view all data of address book.

--- Contact.java
+++ Contact.java
@@ -16,6 +16,62 @@ public class Contact {
 		this.phoneNumber = phoneNumber;
 	}
 
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public int getZip() {
+		return zip;
+	}
+
+	public long getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public void setZip(int zip) {
+		this.zip = zip;
+	}
+
+	public void setPhoneNumber(long phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
 	@Override
 	public String toString() {
 		return "Contact [firstName=" + firstName + ", lastName=" + lastName + ", city=" + city + ", state=" + state
