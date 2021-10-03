27ad1fe68041bd538ac93bb4a1a72c3f3c90d413
Amit Sisodiya <amitsisodiya275@gmail.com>
2021-10-03 11:44:52 +0530

    Added: a new file for UC2 in which added new method that can add new Contact in Address Book.

--- Contact.java
+++ Contact.java
@@ -1,24 +1,24 @@
 package com.bridgelab.addressbook;
 
 public class Contact {
-
 	private String firstName, lastName, city, state, email;
 	private int zip;
 	private long phoneNumber;
 
-	public Contact(String firstName, String lastName, String city, String state, int zip, String email,
+	public Contact(String firstName, String lastName, String city, String state, String email, int zip,
 			long phoneNumber) {
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.city = city;
 		this.state = state;
-		this.zip = zip;
 		this.email = email;
+		this.zip = zip;
 		this.phoneNumber = phoneNumber;
 	}
-	
-	public String toString() {
-		return "First Name : "+firstName+", Last Name : "+lastName+", City : "+city+", State : "+state+", Zip code : "+zip+", Email-ID : "+email+", Phone Number : "+phoneNumber;
-	}
 
+	@Override
+	public String toString() {
+		return "Contact [firstName=" + firstName + ", lastName=" + lastName + ", city=" + city + ", state=" + state
+				+ ", email=" + email + ", zip=" + zip + ", phoneNumber=" + phoneNumber + "]";
+	}
 }
